dd2206e241bf7bebc3d6d62e6ef9f222caa82c8f
Magnus Hagdorn <magnus.hagdorn@ed.ac.uk>
2016-03-16 17:29:50 +0000

    work on connect dialog

--- piccolo2/player/PlayerApp.py
+++ piccolo2/player/PlayerApp.py
@@ -2,12 +2,24 @@ __all__ = ['main']
 
 from PyQt4 import QtGui
 import player
+import connect
 
 class PlayerApp(QtGui.QMainWindow, player.Ui_MainWindow):
     def __init__(self, parent=None):
         super(PlayerApp, self).__init__(parent)
         self.setupUi(self)
 
+        # hook up menu
+        self.action_Connect.triggered.connect(self.connect)
+        self.action_Quit.triggered.connect(QtGui.qApp.quit)
+
+    def connect(self):
+        dialog = QtGui.QDialog()
+        dialog.ui = connect.Ui_ConnectDialog()
+        dialog.ui.setupUi(self)
+        dialog.show()
+
+        
 
 def main():
     app = QtGui.QApplication([])
